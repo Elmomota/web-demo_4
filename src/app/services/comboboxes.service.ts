0fbd9a346c623bf5aef0e5fb1a06edf967ea02c2
vicente <vicerivera.a@gmail.com>
2025-06-24 16:55:29 -0400

    construccion endpoint consumidor de comboboxes

--- src/app/services/comboboxes.service.ts
+++ src/app/services/comboboxes.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComboboxesService {
+  private baseUrl = `${environment.apiUrl}/api/combobox`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTiposUsuario(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/tipos-usuario`);
+  }
+
+  getRegiones(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/regiones`);
+  }
+
+  getComunas(region_id: number): Observable<any[]> {
+    const params = new HttpParams().set('region_id', region_id.toString());
+    return this.http.get<any[]>(`${this.baseUrl}/comunas`, { params });
+  }
+
+  getCategorias(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/categorias`);
+  }
+
+  getMarcas(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/marcas`);
+  }
+
+  getTiposMovimiento(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/movimientos`);
+  }
+
+  getEstadosProyecto(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/estados-proyecto`);
+  }
+
+  getRolesProyecto(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/roles-proyecto`);
+  }
+
+  getAlmacenes(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/almacenes`);
+  }
 }
