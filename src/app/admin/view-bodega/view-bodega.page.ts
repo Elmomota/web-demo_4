fff9727f08aace3808521fae992143c483a6dfc5
vicente <vicerivera.a@gmail.com>
2025-06-24 23:27:46 -0400

    visualizar bodega ok

--- src/app/admin/view-bodega/view-bodega.page.ts
+++ src/app/admin/view-bodega/view-bodega.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavParams} from '@ionic/angular';
+import { AdmBodegaService } from 'src/app/services/adm-bodega.service';
+import { ComboboxesService } from 'src/app/services/comboboxes.service';
+
 
 @Component({
   selector: 'app-view-bodega',
@@ -7,10 +11,59 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class ViewBodegaPage implements OnInit {
+  isDisabled = true;
 
-  constructor() { }
+  
+  bodega: any;
+
+  nombre = '';
+  direccion = '';
+  id_region: number | null = null;
+  id_comuna: number | null = null;
+
+  regiones: any[] = [];
+  comunas: any[] = [];
+
+  constructor(
+      private navParams: NavParams,
+      private modalCtrl: ModalController,
+      private bodegaService: AdmBodegaService,
+      private comboService: ComboboxesService
+  )  { 
+    this.bodega = this.navParams.get('bodega')
+  }
 
   ngOnInit() {
+
+    this.cargarRegiones();
+    this.nombre = this.bodega.nombre;
+    this.direccion = this.bodega.direccion;
+    this.id_comuna = this.bodega.id_comuna;
+    this.id_region = this.bodega.id_region;
+
+    this.comboService.getComunas(this.id_region!).subscribe(res => {
+      this.comunas = res;
+    });
+}
+
+  cargarRegiones() {
+    this.comboService.getRegiones().subscribe(res => {
+      this.regiones = res;
+    });
   }
+  onRegionChange() {
+    if (this.id_region) {
+      this.comboService.getComunas(this.id_region).subscribe(res => {
+        this.comunas = res;
+        this.id_comuna = null;
+      });
+    }
+  }
+
+  cerrar() {
+    this.modalCtrl.dismiss();
+  }
+  
+
 
 }
